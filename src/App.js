82fd0e9f49b119aea7f57617faa0df16f9b8f35c
duongnhat0801 <nhatx0801@hmail.com>
2024-08-19 10:29:57 +0700

    tableStudent

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import './index.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    const students = [
+        {
+            company: 'Alfreds Futterkiste',
+            contact: 'Maria Anders',
+            country: 'Germany'
+        },
+        {
+            company: 'Centro comercial Moctezuma',
+            contact: 'Francisco Chang',
+            country: 'Mexico'
+        },
+        {
+            company: 'Ernst Handel',
+            contact: 'Roland Mendel',
+            country: 'Austria'
+        },
+        {
+            company: 'Island Trading',
+            contact: 'Helen Bennett',
+            country: 'UK'
+        },
+        {
+            company: 'Laughing Bacchus Winecellars',
+            contact: 'Yoshi Tannamuri',
+            country: 'Canada'
+        },
+        {
+            company: 'Magazzini Alimentari Riuniti',
+            contact: 'Giovanni Rovelli',
+            country: 'Italy'
+        }
+    ]
+
+    return (
+   <>
+     {students.map(student => (
+         <tr>
+           <td>{student.company}</td>
+           <td>{student.contact}</td>
+           <td>{student.country}</td>
+         </tr>
+     ))}
+   </>
   );
 }
 
